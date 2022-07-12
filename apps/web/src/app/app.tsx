74e211959debcad94b28917d2ca76133ec9ca521
Doni Rubiagatra <doni@zero-one-group.com>
2022-07-13 06:27:05 +0700

    add react app and explore nx workspace
    Signed-off-by: Doni Rubiagatra <doni@zero-one-group.com>

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <header>
+        <h1>Bookstore</h1>
+      </header>
+    </StyledApp>
+  );
+}
+
+export default App;
